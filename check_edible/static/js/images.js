5931abf989b6b56222578383f389811b6b4ae03b
Hakjun Kim <hukanj@gmail.com>
2020-09-20 14:56:29 +0900

    Implement Django for our homepage. Static assets are readdressed for Django. Keyword searching applied.

--- check_edible/static/js/images.js
+++ check_edible/static/js/images.js
@@ -10,7 +10,7 @@ jQuery(function($){
 					// Components							
 					slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
 					slides 					:  	[			// Slideshow Images
-														{image : 'images/background/01.jpg'}
+														{image : '/static/images/background/01.jpg'}
 												]
 					
 				});
--- check_edible/views.py
+++ check_edible/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def mainpage(request):
+    return render(request, 'mainpage/index.html', {})
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    engine = request.GET.get('engine')
+
+    if engine == 'google':
+        search_url ='https://www.google.co.kr/search?q='+ keyword
+    elif engine == 'naver':
+        search_url = 'https://search.naver.com/search.naver?query=' + keyword
+
+    
+    
+    return HttpResponseRedirect(search_url)
