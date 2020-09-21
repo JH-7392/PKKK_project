cecd009a029a94fd0bc4091a2d6cf0bc7d902bd6
Hakjun Kim <hukanj@gmail.com>
2020-09-21 11:14:47 +0900

    Search feature error modified.

--- check_edible/views.py
+++ check_edible/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def mainpage(request):
@@ -10,10 +10,9 @@ def search(request):
     engine = request.GET.get('engine')
 
     if engine == 'google':
-        search_url ='https://www.google.co.kr/search?q='+ keyword
+        search_url ='https://www.google.co.kr/search?q='+ keyword + '&tbm=isch'
     elif engine == 'naver':
-        search_url = 'https://search.naver.com/search.naver?query=' + keyword
+        search_url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=' + keyword
 
-    
     
     return HttpResponseRedirect(search_url)
